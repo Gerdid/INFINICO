0294f9b8468d0acd7b843e1c229399fbe6898094
Gerhard Mora <gmora@tramgroup.com>
2020-03-12 11:08:09 -0600

    Several fixes

--- PythonMariadb.py
+++ PythonMariadb.py
@@ -35,10 +35,8 @@ except mariadb.Error as e:
 #methods for executing SQL code: execute() and executemany()
 cursor=conn.cursor()
 
-def get_balance(cursor):
-	print("Escanee tarjeta")
+def get_balance(cursor,uid):
 	client=[]
-	uid=read_card()
 	uid_string=uid
 	cursor.execute("SELECT balance FROM test.cliente WHERE uid=?",(uid,))
 	for(uid) in cursor:
@@ -47,8 +45,7 @@ def get_balance(cursor):
 	print("El saldo de la tarjeta ",uid_string," es de:",balance)
 	return uid_string,balance
 
-def read_balance(cursor):
-	#print("Escanee tarjeta")
+def read_balance(cursor,uid):
 	#client=[]
 	uid=read_card()
 	uid_string=uid
@@ -58,32 +55,24 @@ def read_balance(cursor):
 	balance=int(re.search(r'\d+',client).group(0))
 	return balance
 
-def read_points(cursor):
-	#print("Escanee tarjeta")
-	#client=[]
-	uid=read_card()
-	uid_string=uid
+def read_points(cursor,uid):
+	#uid_string=uid
 	cursor.execute("SELECT points FROM test.cliente WHERE uid=?",(uid,))
 	for(uid) in cursor:
 		client=(f"{uid}")
 	points=int(re.search(r'\d+',client).group(0))
 	return points
 
-def read_balance_and_points(cursor):
-	#client=[]
-	uid=read_card()
-	uid_string=uid
-	balance=read_balance(cursor)
-	points=read_points(cursor)
-	return uid_string,balance,points
+def read_balance_and_points(cursor,uid):
+	balance=read_balance(cursor,uid)
+	points=read_points(cursor,uid)
+	return uid,balance,points
 
-def recharge(cursor):
-	uid_string,balance=get_balance(cursor)
+def recharge(cursor,uid):
+	uid_string,balance=get_balance(cursor,uid)
 	print("Introduzca el monto a recargar: ")
 	toCharge=int(input())
 	new_balance=balance+toCharge
-	print(new_balance)
-	print(uid_string)
 	update_balance(cursor,uid_string,new_balance)
 
 def update_balance(cursor,uid_string,new_balance):
@@ -94,13 +83,46 @@ def update_points(cursor,uid_string,new_points):
 	#Actualiza los puntos del uid correspondiente
 	cursor.execute("UPDATE test.cliente SET points=? WHERE uid=?",(new_points,uid_string))
 
+def cobrar(cursor,uid):
+	print("Ingrese total a cobrar: ")
+	ammount=int(input())
+	balance=read_balance(cursor)
+	if(balance>=ammount):
+		new_balance=balance-ammount
+		update_balance(cursor,uid_string,new_balance)
+	else:
+		print("Saldo insuficiente, use otro metodo de pago")
+
+def points_to_balance(cursor,uid):
+	#10 puntos= $1
+	points=read_points(cursor,uid)
+	balance=read_balance(cursor,uid)
+	#new_balance=balance+(points/10)
+	#uid_string,balance,points=read_balance_and_points(cursor)
+	#update_balance(cursor,uid,new_balance)
+	#print(type(balance))
+	#new_balance=balance+(points/10)
+	#new_points=points-points
+	balance_to_add=balance+balance//10
+	rem_points=balance%10
+	update_balance(cursor,uid,balance_to_add)
+	update_points(cursor,uid,rem_points)
+
+def display_card_summary(cursor,uid):
+	#uid,balance,points=read_balance_and_points(cursor)
+	balance=read_balance(cursor,uid)
+	points=read_points(cursor,uid)
+	print("-------------------------------------")
+	print("------------Tarjeta No.--------------")
+	print("-------",uid,"--------")
+	print("SALDO:$ ",balance)
+	print("PUNTOS: ",points)
+	print("-------------------------------------")
 
 def read_card():
 	#Se abre el puerto serial para el ordenar a terminal y recepcion de datos de terminal
 	#Serial toma dos parametros: dispositivo serial y el baudrate
 	print("Escanee tarjeta")
-	#client=[]
-	#uid=read_card()
 	ser=serial.Serial('/dev/ttyACM0',9600)
 	ser.write(value) #Escribe los bytes al puerto. Este debe de ser tipo byte (o compatible como bytearray)
 	ser.flush() #Espera a que todos los datos esten escritos
@@ -110,46 +132,12 @@ def read_card():
 	ser.close() #Se cierra el puerto
 	return uid
 
-def cobrar(cursor):
-	print("Ingrese total a cobrar: ")
-	ammount=int(input())
-	uid_string,balance=read_balance(cursor)
-	if(balance>=ammount):
-		new_balance=balance-ammount
-		update_balance(cursor,uid_string,new_balance)
-	else:
-		print("Saldo insuficiente, use otro metodo de pago")
-
-def points_to_balance(cursor):
-	#10 puntos= $1
-	#uid=read_card()
-	#points=read_points(cursor,uid)
-	#balance=read_balance(cursor,uid)
-	#read_balance_and_points(cursor)
-	#new_balance=balance+(points/10)
-	uid_string,balance,points=read_balance_and_points(cursor)
-	#update_balance(cursor,uid,new_balance)
-	print(type(balance))
-	new_balance=balance+(points/10)
-	new_points=points-points
-	update_balance(cursor,uid_string,new_balance)
-	update_points(cursor,uid_string,new_points)
-
-def display_card_summary(cursor):
-	uid_string,balance,points=read_balance_and_points(cursor)
-	print("-------------------------------------")
-	print("------------Tarjeta No.--------------")
-	print("-------",uid_string,"--------")
-	print("SALDO:$ ",balance)
-	print("PUNTOS: ",points)
-	print("-------------------------------------")
-
-
 while True:
 	print("Introduzca operacion: \n 1.Resumen de tarjeta \n 2.Recargar saldo \n 3.Cobrar \n 4.Convertir puntos a saldo")
 	op=input()
 	if(op=='1'):
-		display_card_summary(cursor)
+		uid=read_card(cursor)
+		display_card_summary(cursor,uid)
 	if(op=='2'):
 		recharge(cursor)
 	if(op=='3'):
